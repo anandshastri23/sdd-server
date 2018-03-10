e4da9326e8236e04304484023a48f5d6de546598
Anand Shastri <anandshastri23@gmail.com>
2018-03-10 10:50:10 -0600

    initial

---
+++ src/main/java/com/sdd/sddserver/SddServerApplication.java
@@ -0,0 +1,12 @@
+package com.sdd.sddserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SddServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SddServerApplication.class, args);
+	}
+}
